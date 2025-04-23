5140950e35f186e581b0e3da27a0c7edac627516
AVDWD <yarkerspl@gmail.com>
2025-04-23 12:18:31 +0300

    Changed program.cs

--- UniversitiScheduleApi/Program.cs
+++ UniversitiScheduleApi/Program.cs
@@ -1,5 +1,10 @@
 using Microsoft.EntityFrameworkCore;
+using UniSchedule.Applications.Services;
+using UniSchedule.Core.Interfaces.ServiceInterfaces;
+using UNISchedule.Applications.Services;
+using UNISchedule.Core.Interfaces.RepositoryInterfaces;
 using UNISchedule.DataAccess;
+using UNISchedule.DataAccess.Repositories;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +18,23 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<UniScheduleDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+//repositories
+builder.Services.AddScoped<ITypeSubjectRepository, TypeSubjectRepository>();
+builder.Services.AddScoped<ISubjectRepository, SubjectRepository>();
+builder.Services.AddScoped<ISubjectAssignmentRepository, SubjectAssignmentRepository>();
+builder.Services.AddScoped<IScheduleRecordRepository, ScheduleRecordRepository>();
+builder.Services.AddScoped<IClassTimeRepository, ClassTimeRepository>();
+builder.Services.AddScoped<IClassroomRepository, ClassroomRepository>();
+builder.Services.AddScoped<IGroupRepository, GroupRepository>();
+//services
+builder.Services.AddScoped<ITypeSubjectService, TypeSubjectService>();
+builder.Services.AddScoped<ISubjectService, SubjectService>();
+builder.Services.AddScoped<ISubjectAssignmentService, SubjectAssignmentService>();
+builder.Services.AddScoped<IScheduleRecordService, ScheduleRecordService>();
+builder.Services.AddScoped<IClassTimeService, ClassTimeService>();
+builder.Services.AddScoped<IClassroomService, ClassroomService>();
+builder.Services.AddScoped<IGroupService, GroupService>();
+
 
 var app = builder.Build();
 
